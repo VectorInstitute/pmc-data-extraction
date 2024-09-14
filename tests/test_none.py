0cc05ef26502f084f66ff7bae092a427e47b1589
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-09-13 21:21:24 -0400

    created placeholder integration and non-integration tests to pass coverage and pre-commit

--- tests/test_none.py
+++
@@ -1,9 +0,0 @@
-"""Placeholder Test."""
-
-
-def test_none():
-    """Placeholder test if no tests are defined.
-
-    For pytest to pass during pre-commit, at least one test must be found and
-    passed.
-    """
