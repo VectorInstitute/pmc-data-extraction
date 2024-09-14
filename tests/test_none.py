6f6ed0e7ec32ade7cb2a886383d1cffa70eeb8c7
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-09-13 20:42:00 -0400

    removed unncessary code checks

---
+++ tests/test_none.py
@@ -0,0 +1,9 @@
+"""Placeholder Test."""
+
+
+def test_none():
+    """Placeholder test if no tests are defined.
+
+    For pytest to pass during pre-commit, at least one test must be found and
+    passed.
+    """
