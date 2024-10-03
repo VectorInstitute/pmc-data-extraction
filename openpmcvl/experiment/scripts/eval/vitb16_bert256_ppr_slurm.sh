ce65f6f695520987105f69b7e206ebbc31c69682
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-03 13:46:14 -0400

    updated slurm scripts

---
+++ openpmcvl/experiment/scripts/eval/vitb16_bert256_ppr_slurm.sh
@@ -0,0 +1,23 @@
+# on rtx6000
+mmlearn_run --multirun hydra.launcher.mem_gb=0 \
+    hydra.launcher.qos=long \
+    hydra.launcher.partition=rtx6000 \
+    hydra.launcher.gres=gpu:1 \
+    hydra.launcher.cpus_per_task=32 \
+    hydra.launcher.tasks_per_node=1 \
+    hydra.launcher.nodes=1 \
+    hydra.launcher.stderr_to_stdout=true \
+    hydra.launcher.timeout_min=2880 \
+    '+hydra.launcher.additional_parameters={export: ALL}' \
+    'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
+    +experiment=biomedclip_ppr \
+    experiment_name=biomedclip_ppr_1pair \
+    job_type=eval \
+    dataloader.test.batch_size=64 \
+    dataloader.test.num_workers=4 \
+    task.encoders.patient_q.pretrained=True \
+    task.encoders.patient_t.pretrained=True \
+    strict_loading=False \
+    resume_from_checkpoint="/checkpoint/yaspar//last.ckpt"
+# comment: test_clean_1 is an experimental split with 400K pairs.
+
--- openpmcvl/experiment/scripts/eval/vitb16_bert256_retrieval_dev.sh
+++ openpmcvl/experiment/scripts/eval/vitb16_bert256_retrieval_dev.sh
@@ -45,8 +45,8 @@ mmlearn_run \
     +experiment=biomedclip_ppr \
     experiment_name=biomedclip_ppr_1pair \
     job_type=eval \
-    dataloader.test.batch_size=8 \
-    dataloader.test.num_workers=2 \
+    dataloader.test.batch_size=64 \
+    dataloader.test.num_workers=4 \
     task.encoders.patient_q.pretrained=True \
     task.encoders.patient_t.pretrained=True \
     trainer.logger.wandb.offline=True
