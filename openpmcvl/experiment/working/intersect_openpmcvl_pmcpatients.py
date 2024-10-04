854ca88bb8dd629d10f3d5979d61599180ff1bee
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-04 19:20:23 -0400

    removed duplicated PMIDs before counting

--- openpmcvl/experiment/working/intersect_openpmcvl_pmcpatients.py
+++ openpmcvl/experiment/working/intersect_openpmcvl_pmcpatients.py
@@ -10,7 +10,7 @@ def get_openpmcvl_article_ids(root_dir: str, split: str):
     with open(os.path.join(root_dir, f"{split}.jsonl")) as file:
         data = [json.loads(line) for line in tqdm(file)]
     pmids = [sample["PMC_ID"].replace("PMC", "") for sample in data]
-    return pmids
+    return list(set(pmids))
 
 def get_pmcpatients_pmids(root_dir):
     # open PMC_IDs and PAR_PMIDs from metadata
@@ -18,16 +18,19 @@ def get_pmcpatients_pmids(root_dir):
         pmids = json.load(file)
     with open(os.path.join(root_dir, "PMC-Patients-MetaData/PAR_PMIDs.json")) as file:
         par_pmids = json.load(file)
-    return pmids, par_pmids
+    return list(set(pmids)), list(set(par_pmids))
 
 if __name__ == "__main__":
     print("Loading PMC-Patients PMIDs...")
     pmcpatients_pmids, pmcpatients_corpus_pmids = get_pmcpatients_pmids(root_dir="/projects/multimodal/datasets/pmc_patients")
+    print(f"Loaded {len(pmcpatients_pmids)} Patient PMIDs and {len(pmcpatients_corpus_pmids)} Corpus PMIDs.")
 
     print("Loading OpenPMC-VL(train+val splits) PMIDs...")
     openpmcvl_pmids = get_openpmcvl_article_ids(root_dir="/datasets/PMC-15M/processed/", split="train_clean")
     openpmcvl_pmids_val = get_openpmcvl_article_ids(root_dir="/datasets/PMC-15M/processed/", split="val_clean")
     openpmcvl_pmids.extend(openpmcvl_pmids_val)
+    openpmcvl_pmids = list(set(openpmcvl_pmids))
+    print(f"Loaded {len(openpmcvl_pmids)} train+val PMIDs.")
 
     # count common articles in pmcpatients and openpmcvl
     print("Counting common articles in both sets...")
@@ -38,10 +41,16 @@ if __name__ == "__main__":
     for id in tqdm(openpmcvl_pmids):
         if id in pmcpatients_pmids:
             counter_patients += 1
-            clean_pmids.remove(id)
+            try:
+                clean_pmids.remove(id)
+            except Exception as e:
+                print(f"Error occured on id={id}: {e}")
         if id in pmcpatients_corpus_pmids:
             counter_corpus += 1
-            clean_corpus_pmids.remove(id)
+            try:
+                clean_corpus_pmids.remove(id)
+            except Exception as e:
+                print(f"Error occured on id={id}: {e}")
     print(f"Number of common PMIDs in PMC-Patients and OpenPMC-VL train+val splits: {counter_patients}")
     print(f"Number of common PMIDs in PMC-Patients Corpus and OpenPMC-VL train+val splits: {counter_corpus}")
     print(f"Number of remaining articles in PMC-Patients after removing the intersection with OpenPMC-VL train+val splits: {len(pmcpatients_pmids) - counter_patients}")
