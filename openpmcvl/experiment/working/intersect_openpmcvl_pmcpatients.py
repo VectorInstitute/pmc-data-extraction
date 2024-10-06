8bb0e99c876cd5750e8676f3d8e38c92c32b8e1a
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-06 19:20:25 -0400

    finding outersections with PMIDs (fix)

--- openpmcvl/experiment/working/intersect_openpmcvl_pmcpatients.py
+++ openpmcvl/experiment/working/intersect_openpmcvl_pmcpatients.py
@@ -4,52 +4,70 @@ import json
 from tqdm import tqdm
 
 
-def get_openpmcvl_article_ids(root_dir: str, split: str):
+def get_openpmcvl_pmc_ids(root_dir: str, split: str):
     """Get a list of PMIDs of articles in OpenPMC-VL dataset."""
-    # load split
     with open(os.path.join(root_dir, f"{split}.jsonl")) as file:
-        data = [json.loads(line) for line in tqdm(file)]
-    pmids = [sample["PMC_ID"].replace("PMC", "") for sample in data]
-    return list(set(pmids))
+        pmids = [json.loads(line)["PMC_ID"] for line in tqdm(file)]
+    return set(pmids)
+
 
 def get_pmcpatients_pmids(root_dir):
-    # open PMC_IDs and PAR_PMIDs from metadata
-    with open(os.path.join(root_dir, "PMC-Patients-MetaData/PMIDs.json")) as file:
-        pmids = json.load(file)
-    with open(os.path.join(root_dir, "PMC-Patients-MetaData/PAR_PMIDs.json")) as file:
-        par_pmids = json.load(file)
-    return list(set(pmids)), list(set(par_pmids))
+    """Load patient PMIDs and PAR_PMIDs from metadata"""
+    pmids = load_json(os.path.join(root_dir, "PMC-Patients-MetaData/PMIDs.json"))
+    par_pmids = load_json(os.path.join(root_dir, "PMC-Patients-MetaData/PAR_PMIDs.json"))
+    return set(pmids), set(par_pmids)
 
 
-if __name__ == "__main__":
+def get_openpmcvl_pmids(root_dir, split):
+    """Load PMIDs of OpenPMC-VL"""
+    return set(load_json(os.path.join(root_dir, f"pmids_{split}.json")))
+
+
+def load_json(filename):
+    """Load json data from file."""
+    with open(filename, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def save_json(data, filename):
+    """Save given data in given file in json format."""
+    with open(filename, "w") as outfile:
+        json.dump(data, outfile)
+    print(f"Saved data in {filename}")
+
+
+def compute_num_patients(root_dir):
+    """Compute number of patients that are extracted from valid articles that don't intersect with OpenPMC-VL train and val splits."""
+    pass
+
+
+def outersect_patients_openpmcvl():
+    """Find and save the difference of article sets in PMCPatients and OpenPMC-VL."""
     print("Loading PMC-Patients PMIDs...")
     pmcpatients_pmids, pmcpatients_corpus_pmids = get_pmcpatients_pmids(root_dir="/projects/multimodal/datasets/pmc_patients")
-    pmcpatients_pmids = set(pmcpatients_pmids)
-    pmcpatients_corpus_pmids = set(pmcpatients_corpus_pmids)
     print(f"Loaded {len(pmcpatients_pmids)} Patient PMIDs and {len(pmcpatients_corpus_pmids)} Corpus PMIDs.")
 
     print("Loading OpenPMC-VL(train+val splits) PMIDs...")
-    openpmcvl_pmids = get_openpmcvl_article_ids(root_dir="/datasets/PMC-15M/processed/", split="train_clean")
-    openpmcvl_pmids_val = get_openpmcvl_article_ids(root_dir="/datasets/PMC-15M/processed/", split="val_clean")
-    openpmcvl_pmids.extend(openpmcvl_pmids_val)
-    openpmcvl_pmids = set(openpmcvl_pmids)
+    openpmcvl_pmids = get_openpmcvl_pmids(root_dir="/datasets/PMC-15M/processed/", split="train_val_clean")
     print(f"Loaded {len(openpmcvl_pmids)} train+val PMIDs.")
 
-    # count common articles in pmcpatients and openpmcvl
-    print("Computing the differece of sets...")
-    clean_pmids = pmcpatients_pmids.difference(openpmcvl_pmids)
-    print(f"Number of valid PMIDs in PMC-Patients that DON'T exist in OpenPMC-VL train+val splits: {len(clean_pmids)}")
-    print(f"Number of common PMIDs in PMC-Patients that DO exist in OpenPMC-VL train+val splits: {len(pmcpatients_pmids) - len(clean_pmids)}")
+    # count different articles in pmcpatients and openpmcvl
+    print("Computing the differece of article sets...")
+    clean_pmcpatients_pmids = pmcpatients_pmids.difference(openpmcvl_pmids)
+    print(f"Number of valid PMIDs in PMC-Patients that DON'T exist in OpenPMC-VL train+val splits: {len(clean_pmcpatients_pmids)}")
+    print(f"Number of common PMIDs in PMC-Patients that DO exist in OpenPMC-VL train+val splits: {len(pmcpatients_pmids) - len(clean_pmcpatients_pmids)}")
 
-    clean_corpus_pmids = pmcpatients_corpus_pmids.difference(openpmcvl_pmids)
-    print(f"Number of valid PMIDs in PMC-Patients Corpus that DON'T exist in OpenPMC-VL train+val splits: {len(clean_corpus_pmids)}")
-    print(f"Number of common PMIDs in PMC-Patients Corpus that DO exist in OpenPMC-VL train+val splits: {len(pmcpatients_corpus_pmids) - len(clean_corpus_pmids)}")
+    clean_pmcpatients_corpus_pmids = pmcpatients_corpus_pmids.difference(openpmcvl_pmids)
+    print(f"Number of valid PMIDs in PMC-Patients Corpus that DON'T exist in OpenPMC-VL train+val splits: {len(clean_pmcpatients_corpus_pmids)}")
+    print(f"Number of common PMIDs in PMC-Patients Corpus that DO exist in OpenPMC-VL train+val splits: {len(pmcpatients_corpus_pmids) - len(clean_pmcpatients_corpus_pmids)}")
 
     # save pmids after exclusion
-    print("Saving PMIDs after exclusion...")
-    with open("/datasets/PMC-15M/PMCPatients_PMIDs_set.json", "w") as outfile:
-        json.dump(list(clean_pmids), outfile)
-    print("Saved data in /datasets/PMC-15M/PMCPatients_PMIDs_set.json")
-    with open("/datasets/PMC-15M/PMCPatients_PAR_PMIDs_set.json", "w") as outfile:
-        json.dump(list(clean_corpus_pmids), outfile)
-    print("Saved data in /datasets/PMC-15M/PMCPatients_PAR_PMIDs_set.json")
+    print("Saving patient PMIDs after exclusion...")
+    save_json(list(clean_pmcpatients_pmids), "/datasets/PMC-15M/PMCPatients_PMIDs.json")
+    save_json(list(clean_pmcpatients_corpus_pmids), "/datasets/PMC-15M/PMCPatients_PAR_PMIDs.json")
+
+
+if __name__ == "__main__":
+    outersect_patients_openpmcvl()
+
