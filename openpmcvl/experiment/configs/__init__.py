456bee0cce9bc20c8ab0818177be9c9bfea4be8a
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-08 17:34:28 -0400

    added deepeyenet dataset

--- openpmcvl/experiment/configs/__init__.py
+++ openpmcvl/experiment/configs/__init__.py
@@ -9,6 +9,7 @@ from omegaconf import MISSING
 from timm.data.transforms import ResizeKeepRatio
 from torchvision import transforms
 
+from openpmcvl.experiment.datasets.deepeyenet import DeepEyeNet
 from openpmcvl.experiment.datasets.mimiciv_cxr import MIMICIVCXR
 from openpmcvl.experiment.datasets.pmcoa import PMCOA
 from openpmcvl.experiment.datasets.pmcvl import PMCVL
--- openpmcvl/experiment/tests/test_datasets.py
+++ openpmcvl/experiment/tests/test_datasets.py
@@ -7,6 +7,7 @@ from mmlearn.datasets.core import Modalities
 from mmlearn.datasets.processors.tokenizers import HFTokenizer
 
 from openpmcvl.experiment.configs import biomedclip_vision_transform
+from openpmcvl.experiment.datasets.deepeyenet import DeepEyeNet
 from openpmcvl.experiment.datasets.pmcoa import PMCOA
 from openpmcvl.experiment.datasets.quilt1m import Quilt
 
@@ -61,7 +62,7 @@ def test_quilt():
     root_dir = os.getenv("QUILT_ROOT_DIR")
     assert (
         root_dir is not None
-    ), "Please set PMCOA root directory in `QUILT_ROOT_DIR` environment variable."
+    ), "Please set Quilt root directory in `QUILT_ROOT_DIR` environment variable."
 
     # test with all subsets and without transform and tokenizer
     split = "val"
@@ -100,6 +101,51 @@ def test_quilt():
         clean_up_tokenization_spaces=False,
     )
     dataset = Quilt(root_dir, split, subsets, transform, tokenizer)
+    sample = dataset[0]
+    assert isinstance(
+        sample[Modalities.TEXT], torch.Tensor
+    ), f"Expected to find `Tensor` in `Modalities.TEXT` but found {type(sample[Modalities.TEXT])}"
+    assert (
+        sample[Modalities.TEXT].size() == torch.Size([256])
+    ), f"Expected `Modalities.TEXT` to have shape {torch.Size([256])} but found {sample[Modalities.TEXT].size()}"
+    assert (
+        sample[Modalities.RGB].size() == torch.Size([3, 224, 224])
+    ), f"Expected `Modalities.RGB` to have shape {torch.Size([3, 224, 224])} but found {sample[Modalities.RGB].size()}"
+
+
+def test_deepeyenet():
+    """Test DeepEyeNet dataset."""
+    root_dir = os.getenv("DEY_ROOT_DIR")
+    assert (
+        root_dir is not None
+    ), "Please set DeepEyeNet root directory in `DEY_ROOT_DIR` environment variable."
+
+    # test without transform and tokenizer
+    split = "test"
+    transform = None
+    tokenizer = None
+    dataset = DeepEyeNet(root_dir, split, transform, tokenizer)
+    sample = dataset[0]
+    assert isinstance(
+        sample[Modalities.TEXT], str
+    ), f"Expected to find `str` in `Modalities.TEXT` but found {type(sample[Modalities.TEXT])}"
+    assert isinstance(
+        sample[Modalities.RGB], torch.Tensor
+    ), f"Expected to find `Tensor` in `Modalities.RGB` but found {type(sample[Modalities.RGB])}"
+    assert (
+        sample[Modalities.RGB].size(0) == 3
+    ), f"Expected `Modalities.RGB` to have 3 channels but found {sample[Modalities.RGB].size(0)}"
+
+    # test with transform and tokenizer
+    transform = biomedclip_vision_transform(image_crop_size=224, job_type="train")
+    tokenizer = HFTokenizer(
+        model_name_or_path="microsoft/BiomedNLP-BiomedBERT-base-uncased-abstract",
+        max_length=256,
+        padding="max_length",
+        truncation=True,
+        clean_up_tokenization_spaces=False,
+    )
+    dataset = DeepEyeNet(root_dir, split, transform, tokenizer)
     sample = dataset[0]
     assert isinstance(
         sample[Modalities.TEXT], torch.Tensor
