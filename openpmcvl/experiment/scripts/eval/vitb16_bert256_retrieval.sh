822b258d98d61a4496dd5e891f8b0617b17a2aec
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-09 20:06:53 -0400

    added med_benchmarking config

--- openpmcvl/experiment/scripts/eval/vitb16_bert256_retrieval.sh
+++ openpmcvl/experiment/scripts/eval/vitb16_bert256_retrieval.sh
@@ -54,6 +54,23 @@ mmlearn_run 'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
     strict_loading=False \
     resume_from_checkpoint=""
 
+# with MIMICIVCXR dataset, med_benchmarking config
+mmlearn_run 'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
+    +experiment=med_benchmarking \
+    experiment_name=med_benchmarking_retrieval_mimicivcxr \
+    job_type=eval \
+    +datasets@datasets.test.mimic=MIMICIVCXR \
+    datasets.test.mimic.split=test \
+    +datasets/transforms@datasets.test.mimic.transform=med_clip_vision_transform \
+    datasets.test.mimic.transform.job_type=eval \
+    +datasets/tokenizers@dataloader.test.collate_fn.batch_processors.text=HFCLIPTokenizer \
+    dataloader.test.batch_size=8 \
+    dataloader.test.num_workers=4 \
+    task.evaluation_tasks.retrieval.task.task_specs.0.top_k=[10,50,200] \
+    task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[10,50,200] \
+    strict_loading=False \
+    resume_from_checkpoint=""
+
 
 # with Quilt dataset
 mmlearn_run 'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
