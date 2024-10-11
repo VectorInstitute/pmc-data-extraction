075cd770caf96cd39df1e9ef714676d52beea00c
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-11 17:35:13 -0400

    created placeholder for modality classifier class

---
+++ openpmcvl/probe/classifier.py
@@ -0,0 +1,24 @@
+"""Classify image into modalities given in [1] using retrieval.
+
+For each image, the similarity of the image embedding with all labels
+in [1] is computed and the highly similar labels are retrieved.
+
+References
+----------
+[1] Garcia Seco de Herrera, A., Muller, H. & Bromuri, S.
+    "Overview of the ImageCLEF 2015 medical classification task."
+    In Working Notes of CLEF 2015 (Cross Language Evaluation Forum) (2015).
+"""
+
+
+class ModalityClassifier():
+    # write something
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # do something
