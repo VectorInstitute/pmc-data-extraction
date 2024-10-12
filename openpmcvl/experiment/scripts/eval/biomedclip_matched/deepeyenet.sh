821b099c737a968f8fad95b45d3d471835297522
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-12 19:33:10 -0400

    deleting unncessary configs and slurm scripts

--- openpmcvl/experiment/scripts/eval/biomedclip_matched/deepeyenet.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/deepeyenet.sh
@@ -18,3 +18,35 @@ mmlearn_run 'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
     task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[10,50,200] \
     strict_loading=False \
     resume_from_checkpoint=""
+
+
+# with DeepEyeNet dataset
+mmlearn_run --multirun hydra.launcher.mem_gb=64 \
+    hydra.launcher.qos=a40_arashaf_multimodal \
+    hydra.launcher.partition=a40 \
+    hydra.launcher.gres=gpu:1 \
+    hydra.launcher.cpus_per_task=8 \
+    hydra.launcher.tasks_per_node=4 \
+    hydra.launcher.nodes=1 \
+    hydra.launcher.stderr_to_stdout=true \
+    hydra.launcher.timeout_min=4320 \
+    '+hydra.launcher.additional_parameters={export: ALL}' \
+    'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
+    +experiment=biomedclip_matched \
+    experiment_name=biomedclip_retrieval_deepeyenet \
+    job_type=eval \
+    ~datasets.test.pmcvl \
+    +datasets@datasets.test.dey=DeepEyeNet \
+    datasets.test.dey.split=test \
+    +datasets/transforms@datasets.test.dey.transform=biomedclip_vision_transform \
+    datasets.test.dey.transform.job_type=eval \
+    dataloader.test.collate_fn.batch_processors.text.max_length=256 \
+    datasets/tokenizers@dataloader.test.collate_fn.batch_processors.text=BiomedCLIPTokenizer \
+    dataloader.test.batch_size=8 \
+    dataloader.test.num_workers=4 \
+    task.encoders.text.pretrained=True \
+    task.encoders.rgb.pretrained=True \
+    task.evaluation_tasks.retrieval.task.task_specs.0.top_k=[10,50,200] \
+    task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[10,50,200] \
+    strict_loading=False \
+    resume_from_checkpoint=""
--- openpmcvl/experiment/scripts/eval/biomedclip_matched/pmcvl.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/pmcvl.sh
@@ -36,3 +36,24 @@ mmlearn_run --multirun hydra.launcher.mem_gb=0 \
     strict_loading=False \
     resume_from_checkpoint=""
 # comment: test_clean_1 is an experimental split with 400K pairs.
+
+
+# with OpenPMC-VL
+mmlearn_run --multirun hydra.launcher.mem_gb=0 \
+    hydra.launcher.qos=a40_arashaf_multimodal \
+    hydra.launcher.partition=a40 \
+    hydra.launcher.gres=gpu:4 \
+    hydra.launcher.cpus_per_task=8 \
+    hydra.launcher.tasks_per_node=4 \
+    hydra.launcher.nodes=1 \
+    hydra.launcher.stderr_to_stdout=true \
+    hydra.launcher.timeout_min=4320 \
+    '+hydra.launcher.additional_parameters={export: ALL}' \
+    'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
+    +experiment=biomedclip_matched \
+    experiment_name=biomedclip_retrieval_pmcvl \
+    job_type=eval \
+    dataloader.test.batch_size=128 \
+    dataloader.test.num_workers=4 \
+    strict_loading=False \
+    resume_from_checkpoint=path/to/checkpoint
