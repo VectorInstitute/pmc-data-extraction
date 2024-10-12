7e84ef0df95bb9cd5dee81b5fe604d5ff1065dcb
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-11 21:53:19 -0400

    loaded model and encoded data in classifier

--- openpmcvl/probe/classifier.py
+++ openpmcvl/probe/classifier.py
@@ -9,12 +9,22 @@ References
     "Overview of the ImageCLEF 2015 medical classification task."
     In Working Notes of CLEF 2015 (Cross Language Evaluation Forum) (2015).
 """
+from typing import Any, Dict, List, Union
+import logging
 import hydra
+from tqdm import tqdm
 from omegaconf import OmegaConf, DictConfig
 
+import lightning as L  # noqa: N812
+import torch
 import torch.nn as nn
+from torch.utils.data.dataloader import DataLoader
+from pytorch_lightning.utilities import rank_zero_only
+from lightning.pytorch.loggers.wandb import WandbLogger
+from transformers.utils.import_utils import is_torch_tf32_available
 
-from mmlearn.conf import JobType, MMLearnConf, hydra_main
+from mmlearn.cli._instantiators import instantiate_datasets, instantiate_loggers
+from mmlearn.conf import hydra_main
 from mmlearn.datasets.core import *  # noqa: F403
 from mmlearn.datasets.processors import *  # noqa: F403
 from mmlearn.modules.encoders import *  # noqa: F403
@@ -25,144 +35,116 @@ from mmlearn.modules.metrics import *  # noqa: F403
 from mmlearn.tasks import *  # noqa: F403
 
 
-# class ModalityClassifier(nn.Module):
-#     """Classify the modality of an image-text pair by retrieval."""
-
-#     def __init__(self, config_name):
-#         """Initialize the module.
-
-#         Parameters
-#         ----------
-#         config_name: str
-#             Path to a yaml file containinng experiment config.
-#             This config is expected to contain an instance of
-#             `ContrastivePretraining` as "task".
-#         """
-#         super().__init__()
-#         # load config
-#         config = OmegaConf.load(config_name)
-#         print(config)
+logger = logging.getLogger(__package__)
 
 
-#         # load model
-#         task = self._instantiate_task(task_name="biomedclip", checkpoint=None)
+class ModalityClassifier(nn.Module):
+    """Classify the modality of an image-text pair by retrieval."""
 
-#         # load dataset
+    def __init__(self, model: L.LightningModule, loader: DataLoader):
+        """Initialize the module.
 
-#         # load taxonomy
-    
+        Parameters
+        ----------
+        model: L.LightningModule
+            Task module loaded from an mmlearn experiment.
+        loader: DataLoader
+            Data loader.
+        """
+        super().__init__()
+        self.model = model
+        self.loader = loader
 
-#     def _instantiate_task(self, task_name="biomedclip", checkpoint=None):
-#         """Instantiate ContrastivePretraining task."""
-#         if task_name == "biomedclip":
-#             return self._instantiate_biomedclip(checkpoint=checkpoint)
-    
+    def encode(self) -> Dict[str, torch.Tensor]:
+        """Embed image and text."""
+        embeddings: Dict[str, Union[torch.Tensor, List[torch.Tensor]]] = {
+            "text_embedding": [],
+            "rgb_embedding": [],
+        }
+        assert isinstance(embeddings["text_embedding"], list)
+        assert isinstance(embeddings["rgb_embedding"], list)
+        for _, batch in tqdm(enumerate(self.loader), total=len(self.loader), desc="encoding"):
+            outputs = self.model(batch)
+            embeddings["text_embedding"].append(outputs["text_embedding"].detach().cpu())
+            embeddings["rgb_embedding"].append(outputs["rgb_embedding"].detach().cpu())
+            # TODO: remove this
+            if _ > 1:
+                break
+        embeddings["text_embedding"] = torch.cat(embeddings["text_embedding"], axis=0).cpu()  # type: ignore[call-overload]
+        embeddings["rgb_embedding"] = torch.cat(embeddings["rgb_embedding"], axis=0).cpu()  # type: ignore[call-overload]
+        assert isinstance(embeddings["text_embedding"], torch.Tensor)
+        assert isinstance(embeddings["rgb_embedding"], torch.Tensor)
+        return embeddings
 
-#     def _instantiate_biomedclip(self, checkpoint=None):
-#         """Instantiate BiomedCLIP encoders and post-processors."""
-#         encoders = {
-#         "text": BiomedCLIPText(
-#             model_name_or_path="microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224",
-#             pretrained=True,
-#             use_all_token_embeddings=False,
-#             freeze_layers=False,
-#             freeze_layer_norm=True,
-#             model_config_kwargs=None,
-#         ),
-#         "rgb": BiomedCLIPVision(
-#             model_name_or_path="microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224",
-#             pretrained=True,
-#             use_all_token_embeddings=False,
-#             freeze_layers=False,
-#             freeze_layer_norm=True,
-#             model_config_kwargs=None,
-#         ),
-#     }
-#     heads = None
-#     postprocessors = {
-#         "norm_and_logit_scale": {
-#             "norm": L2Norm(dim=-1),
-#             "logit_scale": LearnableLogitScaling(
-#                 logit_scale_init=14.285714285714285,
-#                 learnable=True,
-#                 max_logit_scale=100.0,
-#             ),
-#         }
-#     }
-#     modality_module_mapping = {
-#         "text": ModuleKeySpec(
-#             encoder_key=None, head_key=None, postprocessor_key="norm_and_logit_scale"
-#         ),
-#         "rgb": ModuleKeySpec(
-#             encoder_key=None, head_key=None, postprocessor_key="norm_and_logit_scale"
-#         ),
-#     }
-#     optimizer = None
-#     lr_scheduler = None
-#     loss = CLIPLoss(
-#         l2_normalize=False, local_loss=True, gather_with_grad=True, cache_labels=False
-#     )
-#     modality_loss_pairs = None
-#     auxiliary_tasks = None
-#     log_auxiliary_tasks_loss = False
-#     compute_validation_loss = True
-#     compute_test_loss = True
-#     evaluation_tasks = {
-#         "retrieval": EvaluationSpec(
-#             task=ZeroShotCrossModalRetrievalEfficient(
-#                 task_specs=[
-#                     RetrievalTaskSpec(
-#                         query_modality="text",
-#                         target_modality="rgb",
-#                         top_k=[10, 50, 200],
-#                     ),
-#                     RetrievalTaskSpec(
-#                         query_modality="rgb",
-#                         target_modality="text",
-#                         top_k=[10, 50, 200],
-#                     ),
-#                 ]
-#             ),
-#             run_on_validation=False,
-#             run_on_test=True,
-#         )
-#     }
+    def save_embeddings(
+        self, embeddings: Dict[str, torch.Tensor], filename: str = "./embeddings.pt"
+    ) -> None:
+        """Save text and rgb embeddings on disk."""
+        torch.save(embeddings, filename)
+        print(f"Saved embeddings in {filename}")
 
-#     tasl = ContrastivePretraining(
-#         encoders=encoders,
-#         heads=heads,
-#         postprocessors=postprocessors,
-#         modality_module_mapping=modality_module_mapping,
-#         optimizer=optimizer,
-#         lr_scheduler=lr_scheduler,
-#         loss=loss,
-#         modality_loss_pairs=modality_loss_pairs,
-#         auxiliary_tasks=auxiliary_tasks,
-#         log_auxiliary_tasks_loss=log_auxiliary_tasks_loss,
-#         compute_validation_loss=compute_validation_loss,
-#         compute_test_loss=compute_test_loss,
-#         evaluation_tasks=evaluation_tasks,
-#     )
+    def load_embeddings(self, filename: str) -> Any:
+        """Load embeddings from file."""
+        return torch.load(filename, weights_only=True)
 
-#     # load checkpoint
-
-
-
-
-
-#     def forward(self, x):
-#         """Compute the similarity of image-text paris with all keywords."""
+    def forward(self) -> Dict[str, torch.Tensor]:
+        """Compute the similarity of image-text paris with all keywords."""
+        return self.encode()
 
 
 @hydra_main(version_base=None, config_path="pkg://mmlearn.conf", config_name="base_config")
 def main(cfg: DictConfig):
-    # classifier = ModalityClassifier(config_name="openpmcvl/experiment/configs/experiment/biomedclip_matched.yaml")
-    print(OmegaConf.to_yaml(cfg))
+    """Entry point for classification."""
+    L.seed_everything(cfg.seed, workers=True)
+
+    if is_torch_tf32_available():
+        torch.backends.cuda.matmul.allow_tf32 = True
+        if "16-mixed" in cfg.trainer.precision:
+            cfg.trainer.precision = "bf16-mixed"
+
+    logger.info(OmegaConf.to_yaml(cfg, resolve=True))
+
+    # instantiate data loader
+    test_dataset = instantiate_datasets(cfg.datasets.test)
+    assert (
+        test_dataset is not None
+    ), "Test dataset (`cfg.datasets.test`) is required for evaluation."
+    cfg.dataloader.test["sampler"] = None
+    test_loader = hydra.utils.instantiate(
+        cfg.dataloader.test, dataset=test_dataset, sampler=None
+    )
+
+    # setup task module
+    if cfg.task is None or "_target_" not in cfg.task:
+        raise ValueError(
+            "Expected a non-empty config for `cfg.task` with a `_target_` key. "
+            f"But got: {cfg.task}"
+        )
+    logger.info(f"Instantiating task module: {cfg.task['_target_']}")
+    model: L.LightningModule = hydra.utils.instantiate(cfg.task, _convert_="partial")
+    assert isinstance(model, L.LightningModule), "Task must be a `LightningModule`"
+    model.strict_loading = cfg.strict_loading
+
+    # compile model
+    model = torch.compile(model, **OmegaConf.to_object(cfg.torch_compile_kwargs))
+    # logger.info(model)
+
+    # load a checkpoint
+    logger.info(f"Loading model state from: {cfg.resume_from_checkpoint}")
+    checkpoint = torch.load(cfg.resume_from_checkpoint, weights_only=True)
+    model.load_state_dict(checkpoint["state_dict"], strict=False)
+
+    # instantiate classifier
+    classifier = ModalityClassifier(model, test_loader)
+    logger.info(classifier)
+
+    # encode images and texts
+    embeddings = classifier.encode()
+    print(embeddings)
 
 
 
 
 
 if __name__ == "__main__":
-    # do something
     main()
--- openpmcvl/probe/classify.sh
+++ openpmcvl/probe/classify.sh
@@ -1,4 +1,6 @@
 python openpmcvl/probe/classifier.py \
     'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
     +experiment=biomedclip_matched \
-    experiment_name=test
+    experiment_name=test \
+    strict_loading=False \
+    resume_from_checkpoint="/projects/multimodal/checkpoints/openpmcvl/batch_size_tuning/bs_256/epoch18-step62149.ckpt"
