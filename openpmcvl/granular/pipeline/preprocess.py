57344edb4e0ee350b476f0739645b82edbca0047
afallah <adibvafafallahpour@gmail.com>
2024-10-13 14:09:09 -0400

    Increase preprocessing pipeline efficiency.

--- openpmcvl/granular/pipeline/preprocess.py
+++ openpmcvl/granular/pipeline/preprocess.py
@@ -38,10 +38,9 @@ def check_keywords(caption: str, keywords: List[str]) -> Tuple[List[str], bool]:
             - A list of found keywords.
             - A boolean indicating whether any keywords were found.
     """
+    caption_lower = caption.lower()
     found_keywords = [
-        kw
-        for kw in keywords
-        if re.search(r"\b" + re.escape(kw) + r"\b", caption, re.IGNORECASE)
+        kw for kw in keywords if f" {kw.lower()} " in f" {caption_lower} "
     ]
     return found_keywords, bool(found_keywords)
 
@@ -64,20 +63,33 @@ def preprocess_data(
     Raises:
         IOError: If there are issues reading input files or writing the output file.
     """
-    processed_data = []
+    all_processed_data = []
+    missing_figures = {}
+    output_dir = os.path.dirname(output_file)
 
-    for input_file in tqdm(input_files, desc="Processing files"):
+    for input_file in input_files:
         data = load_dataset(input_file, num_datapoints=-1)
+        processed_data = []
+        missing_figures_count = 0
+
         for item in tqdm(data, desc=f"Processing items in {input_file}", leave=False):
             pmc_id = item["PMC_ID"]
             media_id = item["media_id"]
-            image_name = os.path.basename(item["media_url"])
-            image_path = f"{figure_root}/{pmc_id}_{media_id}.jpg/{image_name}"
+            media_name = item["media_name"]
+            media_url = item["media_url"]
+            caption = item["caption"]
+            image_name = os.path.basename(media_url)
+            image_path = f"{figure_root}/{media_name}/{image_name}"
 
-            # Skip if image doesn't exist
+            # Check if image doesn't exist or is not a .jpg file
+            if not image_path.endswith(".jpg"):
+                continue
+
             if not os.path.exists(image_path):
-                if image_path.endswith(".jpg"):
-                    print(f"Image not found: {image_path}")
+                missing_figures_count += 1
+                if input_file not in missing_figures:
+                    missing_figures[input_file] = []
+                missing_figures[input_file].append(image_path)
                 continue
 
             try:
@@ -86,40 +98,55 @@ def preprocess_data(
                 print(f"Error processing image {image_path}: {str(e)}")
                 continue
 
-            found_keywords, has_keywords = check_keywords(item["caption"], keywords)
+            found_keywords, has_keywords = check_keywords(caption, keywords)
 
             processed_item = {
                 "id": f"{pmc_id}_{media_id}",
                 "PMC_ID": pmc_id,
-                "caption": item["caption"],
+                "caption": caption,
                 "image_path": image_path,
                 "width": width,
                 "height": height,
                 "media_id": media_id,
-                "media_url": item["media_url"],
-                "media_name": item["media_name"],
+                "media_url": media_url,
+                "media_name": media_name,
                 "keywords": found_keywords,
                 "is_medical": has_keywords,
             }
             processed_data.append(processed_item)
 
-    save_jsonl(processed_data, output_file)
-    print(f"Processed data saved to {output_file}")
+        # Save processed data for this input file
+        input_filename = os.path.splitext(os.path.basename(input_file))[0]
+        temp_output_file = os.path.join(output_dir, f"{input_filename}_processed.jsonl")
+        save_jsonl(processed_data, temp_output_file)
+        print(
+            f"\nProcessed {len(processed_data)} items from {input_file}. Saved to {temp_output_file}"
+            f"\nTotal missing .jpg images in {input_file}: {missing_figures_count}\n"
+        )
+
+        all_processed_data.extend(processed_data)
+
+    # Merge all processed data and save to the final output file
+    save_jsonl(all_processed_data, output_file)
+    print(f"All processed data merged and saved to {output_file}")
+
+    # Save missing images to a separate JSONL file
+    missing_figures_file = os.path.join(output_dir, "missing_figures.jsonl")
+    save_jsonl(missing_figures, missing_figures_file)
+    print(f"Missing images information saved to {missing_figures_file}")
 
 
-def main():
+def main(args: argparse.Namespace):
     """
     Main function to parse arguments and run the preprocessing pipeline.
 
     Args:
-        None
-
-    Returns:
-        None
-
-    Raises:
-        ArgumentError: If required arguments are missing or invalid.
+        args (argparse.Namespace): Command-line arguments.
     """
+    preprocess_data(args.input_files, args.output_file, args.figure_root, args.keywords)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Preprocess JSONL files for figure caption analysis"
     )
@@ -148,9 +175,4 @@ def main():
     )
 
     args = parser.parse_args()
-
-    preprocess_data(args.input_files, args.output_file, args.figure_root, args.keywords)
-
-
-if __name__ == "__main__":
-    main()
+    main(args)
--- openpmcvl/granular/pipeline/preprocess.sh
+++ openpmcvl/granular/pipeline/preprocess.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -c 6
+#SBATCH -c 8
 #SBATCH --partition=cpu
-#SBATCH --mem=32GB
-#SBATCH --time=8:00:00
+#SBATCH --mem=48GB
+#SBATCH --time=12:00:00
 #SBATCH --job-name=preprocess
 #SBATCH --output=%x-%j.out
 
@@ -11,11 +11,11 @@ source /h/afallah/light/bin/activate
 cd /h/afallah/pmc-data-extraction
 
 INPUT_DIR="/datasets/PMC-15M"
-OUTPUT_FILE="/datasets/PMC-15M/granular/granular.jsonl"
+OUTPUT_FILE="/datasets/PMC-15M/granular/granular_meta.jsonl"
 FIGURE_ROOT="/datasets/PMC-15M/figures"
 
-# Specify which JSONL files to process (space-separated list of numbers)
-JSONL_NUMBERS="0 1"
+# Specify which JSONL files to process
+JSONL_NUMBERS="0 1 2 3 4 5 6 7 8 9 10 11"
 
 # Construct INPUT_FILES string
 INPUT_FILES=""
@@ -27,5 +27,5 @@ stdbuf -oL -eL srun python3 openpmcvl/granular/pipeline/preprocess.py \
   --input_files $INPUT_FILES \
   --output_file $OUTPUT_FILE \
   --figure_root $FIGURE_ROOT \
-  --keywords MRI fMRI CT CAT PET PET-MRI MEG EEG ultrasound X-ray Xray nuclear imaging tracer isotope scan positron EKG spectroscopy radiograph tomography endoscope endoscopy colonoscopy elastography ultrasonic ultrasonography echocardiogram endomicroscopy pancreatoscopy cholangioscopy enteroscopy retroscopy chromoendoscopy sigmoidoscopy cholangiography pancreatography cholangio-pancreatography esophagogastroduodenoscopy \
+  --keywords MRI fMRI CT CAT PET PET-MRI MEG EEG ultrasound X-ray Xray nuclear imaging tracer isotope scan positron EKG spectroscopy radiograph tomography endoscope endoscopy colonoscopy elastography ultrasonic ultrasonography echocardiogram endomicroscopy pancreatoscopy cholangioscopy enteroscopy retroscopy chromoendoscopy sigmoidoscopy cholangiography pancreatography cholangio-pancreatography esophagogastroduodenoscopy radiology pathology histopathology \
   2>&1 | tee -a %x-%j.out
