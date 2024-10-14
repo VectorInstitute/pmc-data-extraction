0e3ad638bddb5d3493b71f48a930cb76fb0dc974
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-14 01:30:48 -0400

    added keyword similarity computation

--- openpmcvl/probe/classifier.py
+++ openpmcvl/probe/classifier.py
@@ -9,7 +9,7 @@ References
     "Overview of the ImageCLEF 2015 medical classification task."
     In Working Notes of CLEF 2015 (Cross Language Evaluation Forum) (2015).
 """
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Optional, Callable
 import logging
 import hydra
 from tqdm import tqdm
@@ -43,7 +43,10 @@ logger = logging.getLogger(__package__)
 class ModalityClassifier(nn.Module):
     """Classify the modality of an image-text pair by retrieval."""
 
-    def __init__(self, model: L.LightningModule, loader: DataLoader):
+    def __init__(self, model: L.LightningModule,
+                 loader: DataLoader,
+                 tokenizer: Callable[[Union[str, List[str]]], Union[torch.Tensor, Dict[str, Any]]],
+                 keywords: Optional[List[str]] = None):
         """Initialize the module.
 
         Parameters
@@ -52,10 +55,45 @@ class ModalityClassifier(nn.Module):
             Task module loaded from an mmlearn experiment.
         loader: DataLoader
             Data loader.
+        keywords: List[str], optional, default=None
+            List of modality keywords. If none is given, keywords in [1] are used.
+
+        References
+        ----------
+        [1] Garcia Seco de Herrera, A., Muller, H. & Bromuri, S.
+            "Overview of the ImageCLEF 2015 medical classification task."
+            In Working Notes of CLEF 2015 (Cross Language Evaluation Forum) (2015).
         """
         super().__init__()
         self.model = model
         self.loader = loader
+        self.tokenizer = tokenizer
+        if keywords is not None:
+            self.keywords = keywords
+        else:
+            self.keywords = self._default_keywords()
+
+    def _default_keywords(self):
+        """Default modality keywords."""
+        return ["radiology",
+                "ultrasound",
+                "magnetic resonance",
+                "computerized tomography",
+                "x-ray",
+                "angiography",
+                "pet",
+                "visible light photography",
+                "endoscopy",
+                "electroencephalography",
+                "electrocardiography",
+                "electromyography",
+                "microscopy",
+                "gene sequence",
+                "chromatography",
+                "chemical structure",
+                "mathematical formula",
+                "non-clinical photos",
+                "hand-drawn sketches"]
 
     def encode(self) -> Dict[str, torch.Tensor]:
         """Embed image and text."""
@@ -81,10 +119,44 @@ class ModalityClassifier(nn.Module):
         embeddings[Modalities.RGB.embedding] = torch.cat(embeddings[Modalities.RGB.embedding], axis=0).cpu()  # type: ignore[call-overload]
         return embeddings
 
+    def compute(self, embeddings: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """Compute similarity scores between image-text pairs and modality keywords."""
+        # embed keywords
+        kword_embeddings = self.embed_keywords()
+        # compute similarity of image to keyword embeddings
+        kword_embeddings = kword_embeddings / torch.norm(kword_embeddings, dim=1, keepdim=True)
+        embeddings[Modalities.RGB.embedding] = embeddings[Modalities.RGB.embedding] / torch.norm(embeddings[Modalities.RGB.embedding], dim=1, keepdim=True)
+        scores = torch.matmul(embeddings[Modalities.RGB.embedding], kword_embeddings.T)
+        return torch.softmax(scores, dim=1)  # num_samples x num_keywords
+
+    def sort_labels(self, scores: torch.Tensor):
+        """Sort keywords based on similarity scores."""
+        sorted_scores, indices = torch.sort(scores, dim=1, descending=True, stable=True)
+        sorted_labels = [[self.keywords[idx] for idx in row] for row in indices]
+        return sorted_labels, sorted_scores
+
+    def embed_keywords(self) -> torch.Tensor:
+        """
+        Generate embeddings for the given classes using BiomedCLIP.
+
+        Returns
+        -------
+        torch.Tensor
+            Tensor of keyword embeddings.
+        """
+        template = "this is a photo of {}"
+        inputs = self.tokenizer([template.format(word) for word in self.keywords])
+        if Modalities.TEXT not in inputs and isinstance(inputs, torch.Tensor):
+            inputs = {Modalities.TEXT: inputs}
+        with torch.no_grad():
+            text_features = self.model.encode(inputs, Modalities.TEXT)
+        return text_features
+
     def save_embeddings_as_csv(self, embeddings: Dict[str, torch.Tensor], filename: str = "./embeddings.csv"):
         """Save text and rgb embeddings along with entries as csv."""
-        for mod in [Modalities.TEXT, Modalities.RGB]:
-            embeddings[mod.embedding] = embeddings[mod.embedding].tolist()
+        for key in embeddings.keys():
+            if isinstance(embeddings[key], torch.Tensor):
+                embeddings[key] = embeddings[key].tolist()
         entries_df = pd.DataFrame.from_dict(embeddings, orient="columns")
         entries_df.to_csv(filename, sep=",")
 
@@ -125,6 +197,8 @@ def main(cfg: DictConfig):
     test_loader = hydra.utils.instantiate(
         cfg.dataloader.test, dataset=test_dataset, sampler=None
     )
+    # instantiate tokenizer
+    test_tokenizer = hydra.utils.instantiate(cfg.dataloader.test.collate_fn.batch_processors.text)
 
     # setup task module
     if cfg.task is None or "_target_" not in cfg.task:
@@ -147,18 +221,33 @@ def main(cfg: DictConfig):
         checkpoint = torch.load(cfg.resume_from_checkpoint, weights_only=True)
         model.load_state_dict(checkpoint["state_dict"], strict=False)
 
+    # setup keywords
+    keywords = None
+
     # instantiate classifier
-    classifier = ModalityClassifier(model, test_loader)
+    classifier = ModalityClassifier(model, test_loader, test_tokenizer, keywords)
 
     # encode images and texts
     embeddings = classifier.encode()
 
-    # save embeddings as csv
-    classifier.save_embeddings(embeddings, "openpmcvl/probe/embeddings.pt")
-    # load embeddings from csv
-    embeddings = classifier.load_embeddings("openpmcvl/probe/embeddings.pt")
+    # # save embeddings
+    # classifier.save_embeddings(embeddings, "openpmcvl/probe/embeddings.pt")
+    # # load embeddings
+    # embeddings = classifier.load_embeddings("openpmcvl/probe/embeddings.pt")
 
-    
+    # compute similarities of images with keywords
+    scores = classifier.compute(embeddings)
+    # sort labels
+    sorted_labels, sorted_scores = classifier.sort_labels(scores)
+
+    print(sorted_labels[0])
+    print(sorted_scores[0])
+
+    # save entries as csv
+    embeddings.pop(Modalities.TEXT.embedding)
+    embeddings.pop(Modalities.RGB.embedding)
+    embeddings.update({"labels": sorted_labels, "scores": sorted_scores})
+    classifier.save_embeddings_as_csv(embeddings, "openpmcvl/probe/embeddings.csv")
 
 
 
