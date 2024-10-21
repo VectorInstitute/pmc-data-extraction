dea224cd92c13757e57058a74f7a698071ea1f2c
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-21 02:10:37 -0400

    matched config of biomedclip

--- openpmcvl/experiment/scripts/train/open_clip/biomedclip_matched.sh
+++ openpmcvl/experiment/scripts/train/open_clip/biomedclip_matched.sh
@@ -1,29 +1,38 @@
 cd openpmcvl/experiment/open_clip
 export PYTHONPATH="$PYTHONPATH:$PWD/src"
 
+
 python -m open_clip_train.main \
-        --model hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224 \
-        --train-data /datasets/PMC-15M/processed/train_dummy_.jsonl \
-        --val-data /datasets/PMC-15M/processed/test_dummy_.jsonl \
+        --model biomedclip_tokenizer \
+        --pretrained-image \
+        --train-data /datasets/PMC-15M/processed/train_cleaner.jsonl \
+        --val-data /datasets/PMC-15M/processed/val_cleaner.jsonl \
         --dataset-type jsonl \
         --data-img-key media_name \
         --data-caption-key caption_name \
         --data-img-rootdir /datasets/PMC-15M/figures/ \
         --data-cap-rootdir /datasets/PMC-15M/captions/ \
-        --train-num-samples 1000 \
-        --val-num-samples 500 \
+        --train-num-samples 13397815 \
+        --val-num-samples 2512082 \
         --val-no-retrieval \
-        --batch-size 32 \
-        --accum-freq 1 \
-        --aug-cfg quilt_crop=True \
+        --batch-size 256 \
+        --accum-freq 4 \
         --workers 4 \
-        --lr 1e-4 \
+        --lr 5e-4 \
+        --beta1 0.9 \
+        --beta1 0.98 \
+        --eps 1e-6 \
         --lr-scheduler cosine \
-        --epochs 100 \
-        --warmup 10 \
-        --wd 0.1 \
+        --wd 0.2 \
+        --epochs 32 \
+        --warmup 2000 \
+        --seed 0 \
+        --image-mean 0.48145466 0.4578275 0.40821073 \
+        --image-std 0.26862954 0.26130258 0.27577711 \
+        --val-frequency 1 \
+        --precision amp_bfloat16 \
+        --local-loss \
         --name test \
         --resume latest \
-        --gather-with-grad \
         --logs /checkpoint/$USER/$SLURM_JOBID/ \
-        --zeroshot-frequency 1
+        --report-to wandb
--- openpmcvl/experiment/scripts/train/open_clip/dev.sh
+++ openpmcvl/experiment/scripts/train/open_clip/dev.sh
@@ -27,6 +27,42 @@ python -m open_clip_train.main \
 
 
 python -m open_clip_train.main \
+        --model biomedclip_tokenizer \
+        --pretrained-image \
+        --train-data /datasets/PMC-15M/processed/train_dummy_.jsonl \
+        --val-data /datasets/PMC-15M/processed/test_dummy_.jsonl \
+        --dataset-type jsonl \
+        --data-img-key media_name \
+        --data-caption-key caption_name \
+        --data-img-rootdir /datasets/PMC-15M/figures/ \
+        --data-cap-rootdir /datasets/PMC-15M/captions/ \
+        --train-num-samples 100 \
+        --val-num-samples 500 \
+        --val-no-retrieval \
+        --batch-size 4 \
+        --accum-freq 4 \
+        --workers 4 \
+        --lr 5e-4 \
+        --beta1 0.9 \
+        --beta1 0.98 \
+        --eps 1e-6 \
+        --lr-scheduler cosine \
+        --wd 0.2 \
+        --epochs 32 \
+        --warmup 2000 \
+        --seed 0 \
+        --image-mean 0.48145466 0.4578275 0.40821073 \
+        --image-std 0.26862954 0.26130258 0.27577711 \
+        --val-frequency 1 \
+        --precision amp_bfloat16 \
+        --local-loss \
+        --name test \
+        --resume latest \
+        --logs /checkpoint/$USER/$SLURM_JOBID/ \
+        --report-to wandb
+
+
+python -m open_clip_train.main \
     --val-data $PMCOA_ROOT_DIR/valid.csv \
     --dataset-type csv \
     --data-separator , \
