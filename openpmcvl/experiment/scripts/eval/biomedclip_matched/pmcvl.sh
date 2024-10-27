7bb46dbca1748bcc88f87878de0b84f7579b5af0
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-10-26 20:53:22 -0400

    added pmcvl retrieval scripts

--- openpmcvl/experiment/scripts/eval/biomedclip_matched/pmcvl.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/pmcvl.sh
@@ -1,8 +1,8 @@
 mmlearn_run --multirun hydra.launcher.mem_gb=0 \
-    hydra.launcher.qos=a40_arashaf_multimodal \
-    hydra.launcher.partition=a40 \
+    hydra.launcher.qos=a100_arashaf \
+    hydra.launcher.partition=a100 \
     hydra.launcher.gres=gpu:4 \
-    hydra.launcher.cpus_per_task=8 \
+    hydra.launcher.cpus_per_task=4 \
     hydra.launcher.tasks_per_node=4 \
     hydra.launcher.nodes=1 \
     hydra.launcher.stderr_to_stdout=true \
@@ -15,7 +15,7 @@ mmlearn_run --multirun hydra.launcher.mem_gb=0 \
     datasets.test.pmcvl.split=test_cleaner \
     dataloader.test.batch_size=64 \
     dataloader.test.num_workers=2 \
-    'task.evaluation_tasks.retrieval.task.task_specs=[{query_modality:text,target_modality:rgb,top_k:[1,5,10]}]' \
+    'task.evaluation_tasks.retrieval.task.task_specs=[{query_modality:rgb,target_modality:text,top_k:[1,5,10]}]' \
     strict_loading=False \
     resume_from_checkpoint=""
 
