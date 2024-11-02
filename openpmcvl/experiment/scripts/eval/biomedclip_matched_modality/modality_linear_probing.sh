d01f68375a6ef14974168bedec0ccad05499801c
Negiiiin <neginbaghbanzadeh@gmail.com>
2024-11-01 21:40:20 -0400

    WIP - some modality classification changes

--- openpmcvl/experiment/scripts/eval/biomedclip_matched_modality/modality_linear_probing.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched_modality/modality_linear_probing.sh
@@ -1,6 +1,6 @@
 mmlearn_run --multirun hydra.launcher.mem_gb=64 \
-    hydra.launcher.qos=normal \
-    hydra.launcher.partition=rtx6000 \
+    hydra.launcher.qos=a40_arashaf_multimodal \
+    hydra.launcher.partition=a40 \
     hydra.launcher.gres=gpu:1 \
     hydra.launcher.cpus_per_task=4 \
     hydra.launcher.tasks_per_node=1 \
@@ -10,13 +10,37 @@ mmlearn_run --multirun hydra.launcher.mem_gb=64 \
     '+hydra.launcher.additional_parameters={export: ALL}' \
     'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
     +experiment=modality_linear_probing \
-    experiment_name=biomedclip_matched_modality_linear_probing \
-    task.num_classes=3 \
-    task.lr_scheduler.scheduler.T_max=2480 \
-    job_type=eval \
-    ~task.postprocessors.norm_and_logit_scale.norm \
-    task.encoder.rgb.pretrained=True \
+    experiment_name=5_modality_imagenet \
+    task.num_classes=5 \
+    task.lr_scheduler.scheduler.T_max=2445 \
+    job_type=train \
+    ~task.postprocessors.norm \
+    task.encoder.rgb.pretrained=False \
     task.postprocessors.logit_scale.logit_scale_init=4.4454 \
-    task.postprocessors.logit_scale.learnable=False \
+    task.postprocessors.logit_scale.learnable=True \
     task.encoder_checkpoint_path="/projects/DeepLesion/projects/pmc-data-extraction/outputs/biomedclip_matched_modality_linear_probing/2024-10-28/12-55-15/0_13808429/multimodal/bqe0tmpl/checkpoints/epoch\=39-step\=37080.ckpt" \
+
+
+
+
+
+
+mmlearn_run --multirun hydra.launcher.mem_gb=64 \
+    hydra.launcher.qos=a40_arashaf_multimodal \
+    hydra.launcher.partition=a40 \
+    hydra.launcher.gres=gpu:1 \
+    hydra.launcher.cpus_per_task=4 \
+    hydra.launcher.tasks_per_node=1 \
+    hydra.launcher.nodes=1 \
+    hydra.launcher.stderr_to_stdout=true \
+    hydra.launcher.timeout_min=600 \
+    '+hydra.launcher.additional_parameters={export: ALL}' \
+    'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
+    +experiment=modality_classifier_linear_probing \
+    experiment_name=5_modality_imagenet \
+    task.num_classes=5 \
+    task.lr_scheduler.scheduler.T_max=2445 \
+    job_type=train \
+    task.encoder_checkpoint_path="/projects/DeepLesion/projects/pmc-data-extraction/outputs/biomedclip_matched_modality_linear_probing/2024-10-28/12-55-15/0_13808429/multimodal/bqe0tmpl/checkpoints/epoch\=39-step\=37080.ckpt" \
+
 
