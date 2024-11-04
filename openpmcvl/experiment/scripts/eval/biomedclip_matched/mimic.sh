967db391bcd30a27ed813444c1f6f065dbdae9db
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-11-03 19:53:56 -0500

    updated biomedclip_matched scripts

--- openpmcvl/experiment/scripts/eval/biomedclip_matched/mimic.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/mimic.sh
@@ -21,8 +21,8 @@ mmlearn_run --multirun hydra.launcher.mem_gb=64 \
     dataloader.test.num_workers=4 \
     task.postprocessors.norm_and_logit_scale.logit_scale.logit_scale_init=4.4454 \
     task.postprocessors.norm_and_logit_scale.logit_scale.learnable=False \
-    ~task.postprocessors.norm_and_logit_scale.norm \
     task.evaluation_tasks.retrieval.task.task_specs.0.top_k=[10,50,200] \
     task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[10,50,200] \
+    ~task.postprocessors.norm_and_logit_scale.norm \
     strict_loading=True \
     resume_from_checkpoint=""
--- openpmcvl/experiment/scripts/eval/biomedclip_matched/quilt.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/quilt.sh
@@ -1,9 +1,9 @@
 mmlearn_run --multirun hydra.launcher.mem_gb=64 \
     hydra.launcher.qos=a40_arashaf_multimodal \
     hydra.launcher.partition=a40 \
-    hydra.launcher.gres=gpu:2 \
+    hydra.launcher.gres=gpu:1 \
     hydra.launcher.cpus_per_task=8 \
-    hydra.launcher.tasks_per_node=2 \
+    hydra.launcher.tasks_per_node=1 \
     hydra.launcher.nodes=1 \
     hydra.launcher.stderr_to_stdout=true \
     hydra.launcher.timeout_min=600 \
@@ -21,8 +21,8 @@ mmlearn_run --multirun hydra.launcher.mem_gb=64 \
     dataloader.test.num_workers=4 \
     task.postprocessors.norm_and_logit_scale.logit_scale.logit_scale_init=4.4454 \
     task.postprocessors.norm_and_logit_scale.logit_scale.learnable=False \
-    ~task.postprocessors.norm_and_logit_scale.norm \
     task.evaluation_tasks.retrieval.task.task_specs.0.top_k=[10,50,200] \
     task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[10,50,200] \
+    ~task.postprocessors.norm_and_logit_scale.norm \
     strict_loading=False \
     resume_from_checkpoint=""
