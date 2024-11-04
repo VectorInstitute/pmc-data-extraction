b6b948abca4c90a8e6332f6a00d929c71093100e
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-11-03 19:36:27 -0500

    added biomedclip logit_scale to zs script

--- openpmcvl/experiment/scripts/eval/biomedclip_matched/zero_shot_classification.sh
+++ openpmcvl/experiment/scripts/eval/biomedclip_matched/zero_shot_classification.sh
@@ -13,7 +13,10 @@ mmlearn_run --multirun hydra.launcher.mem_gb=80 \
     experiment_name=vitb16_bert256_zeroshot_bs256_matched_nw4 \
     dataloader.val.batch_size=32 \
     dataloader.val.num_workers=4 \
+    task.postprocessors.norm_and_logit_scale.logit_scale.logit_scale_init=4.4454 \
+    task.postprocessors.norm_and_logit_scale.logit_scale.learnable=False \
     task.encoders.text.pretrained=False \
     task.encoders.rgb.pretrained=False \
+    ~task.postprocessors.norm_and_logit_scale.norm \
     strict_loading=True \
     resume_from_checkpoint="/projects/multimodal/checkpoints/openpmcvl/batch_size_tuning/bs_256/epoch\=31-step\=104672.ckpt"
