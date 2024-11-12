d8e32769cac38c0cd20295ee4e4fd7658f38a511
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-11-12 02:03:14 -0500

    started training and eval on biomedclip

--- openpmcvl/experiment/scripts/eval/vitb16_bert256_ppr_slurm.sh
+++ openpmcvl/experiment/scripts/eval/vitb16_bert256_ppr_slurm.sh
@@ -48,34 +48,7 @@ mmlearn_run --multirun hydra.launcher.mem_gb=64 \
     strict_loading=False \
     resume_from_checkpoint="/projects/multimodal/checkpoints/openpmcvl/batch_size_tuning/bs_256/epoch\=18-step\=62149.ckpt"
 
-# eval on rtx6000 biomedclip
-mmlearn_run --multirun hydra.launcher.mem_gb=64 \
-    hydra.launcher.qos=normal \
-    hydra.launcher.partition=rtx6000 \
-    hydra.launcher.gres=gpu:1 \
-    hydra.launcher.cpus_per_task=8 \
-    hydra.launcher.tasks_per_node=1 \
-    hydra.launcher.nodes=1 \
-    hydra.launcher.stderr_to_stdout=true \
-    hydra.launcher.timeout_min=900 \
-    '+hydra.launcher.additional_parameters={export: ALL}' \
-    'hydra.searchpath=[pkg://openpmcvl.experiment.configs]' \
-    +experiment=biomedclip_ppr \
-    experiment_name=biomedclip_ppr_eval \
-    job_type=eval \
-    dataloader.test.batch_size=64 \
-    dataloader.test.num_workers=4 \
-    task.encoders.patient_q.pretrained=True \
-    task.encoders.patient_t.pretrained=True \
-    task.encoders.patient_q.clip_ckpt="" \
-    task.encoders.patient_t.clip_ckpt="" \
-    task.evaluation_tasks.retrieval.task.task_specs.0.top_k=[1000] \
-    task.evaluation_tasks.retrieval.task.task_specs.1.top_k=[1000] \
-    task.postprocessors.norm_and_logit_scale.logit_scale.logit_scale_init=4.4454 \
-    task.postprocessors.norm_and_logit_scale.logit_scale.learnable=False \
-    ~task.postprocessors.norm_and_logit_scale.norm \
-    strict_loading=True \
-    resume_from_checkpoint=""
+
 
 # eval on rtx6000
 mmlearn_run --multirun hydra.launcher.mem_gb=64 \
