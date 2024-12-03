5255e4f38945fc5b8a0bf67f1fcc6aa47bd6c813
yasamanparhizkar <yasaman.parhizkar@gmail.com>
2024-12-03 13:29:43 -0500

    small changes in run.sh

--- openpmcvl/foundation/run.sh
+++ openpmcvl/foundation/run.sh
@@ -8,10 +8,10 @@ source ~/Documents/envs/openpmcvl/bin/activate
 # comm license
 nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/comm --license-type comm --volumes 0 > /datasets/PMC-15M/comm/output_v0.txt
 # noncomm license
-nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/non_comm2 --license-type noncomm --volumes 0 > output.txt
-nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/non_comm2 --license-type noncomm --volumes 1 > /datasets/PMC-15M/non_comm2/output_v1.txt
+nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/non_comm --license-type noncomm --volumes 0 > output.txt
+nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/non_comm --license-type noncomm --volumes 1 > /datasets/PMC-15M/non_comm/output_v1.txt
 # other license
 nohup python -u src/fetch_oa.py --num-retries 5 --extraction-dir /datasets/PMC-15M/other --license-type other --volumes 0 > /datasets/PMC-15M/other/output_v0.txt
 
 # dev
-python -u src/fetch_oa.py --num-retries 5 --extraction-dir ./PMC_OA2 --license-type noncomm --volumes 0
+python -u src/fetch_oa.py --num-retries 5 --extraction-dir ./PMC_OA2 --license-type noncomm --volumes 1 2 3
