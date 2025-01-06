c8bba1d3992f1082cf18801987cdb95005843c70
afallah <adibvafafallahpour@gmail.com>
2025-01-05 23:33:02 -0500

    pipeline/subcaption.py

--- openpmcvl/granular/pipeline/subcaption.sh
+++ openpmcvl/granular/pipeline/subcaption.sh
@@ -6,14 +6,16 @@
 #SBATCH --job-name=subcaption
 #SBATCH --output=%x-%j.out
 
+# Activate the environment
 source /h/afallah/light/bin/activate
 
+# Set the working directory
 cd /h/afallah/pmc-data-extraction
 
+# Run the subcaption script
 stdbuf -oL -eL srun python3 openpmcvl/granular/pipeline/subcaption.py \
   --input-file /datasets/PMC-15M/experimental/demo/demo.jsonl \
   --output-file /datasets/PMC-15M/experimental/demo/demo_caption.jsonl \
-  --system-prompt-file openpmcvl/granular/prompts/subcaption_system_prompt.txt \
   --base-url http://gpu030:8080/v1 \
   --model /model-weights/Meta-Llama-3.1-8B-Instruct \
   --max-tokens 500 \
